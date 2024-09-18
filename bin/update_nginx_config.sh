66fbc7e0d072f60e8db43f3909e74e2ff7c032d0
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-09-18 15:00:05 -0400

    Update script to configure nginx on web server

--- bin/update_nginx_config.sh
+++ bin/update_nginx_config.sh
@@ -6,17 +6,20 @@
 # `o pipefail`	Ensure Bash pipelines (for example, cmd | othercmd) return a non-zero status if any of the commands fail
 set -euxo pipefail
 
-scp default.conf ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com:/tmp/default.conf
+MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP=ec2-18-223-186-177.us-east-2.compute.amazonaws.com
 
-# connect to meredith-deploy-playground ec2 instance and run commands in EOF block
-ssh ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com << 'EOF'
+scp flaskr-nginx-server-block ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP}:/tmp/flaskr-nginx-server-block
+
+ssh ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP} << 'EOF'
 set -euo pipefail
 
-echo "Move nginx default.conf file to /etc/nginx/conf.d/ dir"
-sudo mv /tmp/default.conf /etc/nginx/conf.d/
+echo "Configure nginx server block for flaskr"
+sudo mv /tmp/flaskr-nginx-server-block /etc/nginx/sites-available/flaskr
+sudo ln -s /etc/nginx/sites-available/flaskr /etc/nginx/sites-enabled
+
+echo "Check for nginx syntax errors"
+sudo nginx -t
 
 echo "Reload and verify nginx configuration"
-sudo nginx -s reload
-sudo nginx -t
-sudo systemctl status nginx
+sudo systemctl restart nginx
 EOF
