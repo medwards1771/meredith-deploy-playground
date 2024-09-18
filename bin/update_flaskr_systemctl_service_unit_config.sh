06718caa6a02fc75511a3b9dc8f83d1602f3bf4a
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-09-18 14:59:31 -0400

    Update script to configure flaskr.service on web server

--- bin/update_flaskr_systemctl_service_unit_config.sh
+++ bin/update_flaskr_systemctl_service_unit_config.sh
@@ -1,3 +1,5 @@
+# Run from local machine to update flaskr.service on meredith-deployment-playground-web-server
+
 #!/bin/bash
 
 # `e`	        Exit script immediately if any command returns a non-zero exit status
@@ -6,20 +8,21 @@
 # `o pipefail`	Ensure Bash pipelines (for example, cmd | othercmd) return a non-zero status if any of the commands fail
 set -euxo pipefail
 
-scp bin/flaskr.service ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com:/tmp/flaskr.service
+MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP=ec2-18-223-186-177.us-east-2.compute.amazonaws.com
 
-# connect to meredith-deploy-playground ec2 instance and run commands in EOF block
-ssh ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com << 'EOF'
+scp bin/flaskr.service ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP}:/tmp/flaskr.service
+
+ssh ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP} << 'EOF'
 set -euo pipefail
 
 echo "Move systemd unit file to Ubuntu init system"
 sudo mv /tmp/flaskr.service /etc/systemd/system/flaskr.service
 
-echo "stop flaskr, reload daemons, start flaskr, and enable flaskr since flaskr.service file changed on disk"
+echo "stop flaskr, reload daemons, enable flaskr, and start flaskr since flaskr.service file changed on disk"
 sudo systemctl stop flaskr
 sudo systemctl daemon-reload
-sudo systemctl start flaskr
 sudo systemctl enable flaskr
+sudo systemctl start flaskr
 
 echo "Check flaskr service status"
 sudo systemctl status flaskr
