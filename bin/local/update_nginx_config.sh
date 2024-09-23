a9db038184279d537a5a02ff391fa597a7fb2560
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-09-23 09:59:36 -0400

    Rename nginx server block for flaskr

--- bin/local/update_nginx_config.sh
+++ bin/local/update_nginx_config.sh
@@ -8,14 +8,14 @@ set -euxo pipefail
 
 MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP=ec2-18-223-186-177.us-east-2.compute.amazonaws.com
 
-scp bin/local/flaskr-nginx-server-block ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP}:/tmp/flaskr-nginx-server-block
+scp bin/local/nginx-server-block-flaskr ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP}:/tmp/nginx-server-block-flaskr
 scp bin/local/nginx.conf ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP}:/tmp/nginx.conf
 
 ssh ubuntu@${MEREDITH_DEPLOY_PLAYGROUND_WEB_SERVER_PUBLIC_IP} << 'EOF'
 set -euxo pipefail
 
 echo "Configure nginx server block for flaskr"
-sudo mv /tmp/flaskr-nginx-server-block /etc/nginx/sites-available/flaskr
+sudo mv /tmp/nginx-server-block-flaskr /etc/nginx/sites-available/flaskr
 sudo ln -s /etc/nginx/sites-available/flaskr /etc/nginx/sites-enabled || echo "symbolic link already created"
 
 echo "Update /etc/nginx/nginx.conf file"
