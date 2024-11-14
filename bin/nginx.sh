08fe7c63e39be218b616861397fca777fff5b774
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-11-14 12:22:10 -0500

    Only let deploy step succeed if nginx passes healthcheck

--- bin/nginx.sh
+++ bin/nginx.sh
@@ -4,8 +4,24 @@
 # `u`	        Exit script immediately if an undefined variable is used
 # `x`	        Expand and print each command before executing
 # `o pipefail`	Ensure Bash pipelines (for example, cmd | othercmd) return a non-zero status if any of the commands fail
-set -euxo pipefail
+set -euo pipefail
 
 echo "Configure nginx web server production"
 
 docker compose up --detach --pull always nginx
+
+echo "Wait 10s before performing healthcheck"
+sleep 10
+
+HEALTH_STATUS=$(docker inspect --format '{{.State.Health.Status}}' "meredith-deploy-playground-nginx-1")
+
+if [[ $HEALTH_STATUS == "healthy" ]]; then
+  echo "Container is healthy. Deployment successful."
+  exit 0
+elif [[ $HEALTH_STATUS == "unhealthy" ]]; then
+  echo "Container is unhealthy. Aborting deployment."
+  exit 1
+elif [[ $HEALTH_STATUS == "starting" ]]; then
+  echo "Container starting"
+  exit 2
+fi
