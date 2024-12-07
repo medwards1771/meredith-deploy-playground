fcaa6556cea24d7e9a013a69245888c6aa10ab38
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-12-07 13:12:00 -0500

    [TEST] Create postgres database with Docker

---
+++ bin/database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# `e`	        Exit script immediately if any command returns a non-zero exit status
+# `u`	        Exit script immediately if an undefined variable is used
+# `x`	        Expand and print each command before executing
+# `o pipefail`	Ensure Bash pipelines (for example, cmd | othercmd) return a non-zero status if any of the commands fail
+set -euxo pipefail
+
+echo "Start database hell yeah legggooooo"
+
+docker compose up --detach database
