a3f41bbdd349f6817959494528423cfcb9aa389a
Meredith Edwards <meredith.edwards1771@gmail.com>
2025-01-30 15:44:14 -0500

    Add redis python client, expose port 6379 to host machine

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Let's see that curl one more time</p>"
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+@app.route("/redis")
+def set_and_get():
+    redis_client.set('foo', 'bar')
+    response = redis_client.get('foo')
+    return response
